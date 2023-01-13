37bdd6042a3a4c008ccccbdc9686f41213677c17
Hannah Baum <baum.219@osu.edu>
2023-01-13 13:29:22 -0500

    Stub file added for bim

---
+++ bim.py
@@ -0,0 +1,12 @@
+
+
+# Algorithm:
+# 1. Solve linear inverse problem using Born
+
+# 2. Solve scattering problem at object and observation points
+
+# 3. a) Substitute field in 2 into integrand in integral eq and 
+#    b) Solve inverse problem to recover next order distribution ftn
+
+# 4. Repeat 2 and compare fields with measured data. If difference < 5% 
+#    of scattered field, terminate successfully. Otherwise repeat until soln converges
