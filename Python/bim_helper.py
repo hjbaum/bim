2fc07d7f0486b29977e9829684d88cc18534b22e
Hannah Baum <baum.219@osu.edu>
2023-04-10 21:50:06 -0400

    Trying to resolve L vs M confusion by removing L

--- Python/bim_helper.py
+++ Python/bim_helper.py
@@ -1,8 +1,11 @@
 import numpy as np
+import numpy.matlib
 import scipy as sci
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
-global K, Es, epsilon
-global parameters, node_data
+global K, Es, epsilon, gs_k
+global parameters, node_data, antenna_data
 
 # BIM Algorithm:
 # 1. Solve linear inverse problem for first order using Born approximation
@@ -12,6 +15,13 @@ global parameters, node_data
 # 4. Repeat 2 and compare fields with measured data. If difference < 5% 
 #    of scattered field, terminate successfully. Otherwise repeat until soln converges
 
+class Antenna:
+    id = 0
+    coord = np.array([0.0,0.0])
+    def __init__(self, id:int, x : float, y:float):
+        self.id = id
+        self.coord = np.array([float(x),float(y)])
+    
 class Node:
     id = 0
     Ez = 0.0
@@ -33,26 +43,27 @@ class Params:
     J = 0 # number of points along y axis
     N = 0 # number of pixels
     M = 1 # number of independent measurements
-    L = 1 # number of sources
     eps_rb = 0
-    eps_min = 0
+    eps_min = 40
     eps_max = 0
     sig_rb = 0
     sig_min = 0
     sig_max = 0
     misf = 0 # Number of iterations for bim sets; Can be initialized dynamically as well
     
+    # eps_min is 40 in the homogeneous domain
+
     def __init__(self, freq: int, dx : float, dy : float, I : int, J : int):
         self.freq = freq
-        self.omega = 2 * np.pi * freq
-        self.k = self.omega/3e8
-        self.dx = dx # distance between points along x axis
-        self.dy = dy # distance between points along y axis
-        self.I = I  # number of points along x axis
-        self.J = J # number of points along y axis
+        self.omega = 2 * np.pi * int(freq)
+        self.k = float(self.omega)/3e8
+        self.dx = float(dx) # distance between points along x axis
+        self.dy = float(dy) # distance between points along y axis
+        self.I = int(I)  # number of points along x axis
+        self.J = int(J) # number of points along y axis
         self.N = int(I*J) # number of pixels
 
-    #def __init__(self, freq, dx, dy, I, J, L, M, eps_rb, eps_min, eps_max, sig_rb, sig_min, sig_max):
+    #def __init__(self, freq, dx, dy, I, J, M, eps_rb, eps_min, eps_max, sig_rb, sig_min, sig_max):
     #    self.freq = freq
     #    self.omega = 2 * np.pi * freq
     #    self.k = self.omega/3e8
@@ -62,7 +73,6 @@ class Params:
     #    self.J = J # number of points along y axis
     #    self.N = I*J # number of pixels
     #    self.M = M # number of independent measurements
-    #    self.L = L # number of sources
     #    self.eps_rb = eps_rb
     #    self.eps_min = eps_min
     #    self.eps_max = eps_max
@@ -77,6 +87,8 @@ Es = []
 epsilon = []
 node_data = []
 
+# Remember to subtract background medium (E matrix of constant value)
+# Use circle example from Boyu - coordinate.
 
 # Calculate Green's function (if not given)
 # r_obj, r_src: coordinate array (x,y,z) for observation and source points
@@ -84,8 +96,9 @@ def greens(r_obs, r_src):
     global parameters
 
     # Scalar distance value
-    R = np.sqrt((r_obs(1)-r_src(1))^2 + (r_obs(2)-r_src(2))^2 + (r_obs(3)-r_src(3))^2)
-    gs = 1j/4 * sci.hankel1(0,parameters.k * R)
+    R = np.sqrt(float((r_obs[0]-r_src[0])**2 + (r_obs[1]-r_src[1])**2))
+
+    gs = 1j/4 * sci.special.hankel1(0,parameters.k * R)
     return gs
 
 # Discretized version of the dyadic Green function
@@ -93,9 +106,9 @@ def greens_disc(r_m, r_n):
     global parameters
     
     an = np.sqrt(parameters.dx*parameters.dy/np.pi)
-    J1 = sci.jv(1,parameters.k*an) # first-order bessel function
-    R = np.sqrt((r_m[0]-r_n[0])^2 + (r_m[1]-r_n[1])^2) # distance vector
-    H0_2 = sci.hankel2(0, parameters.k * R) # 0th-order, 2nd degree hankel function
+    J1 = sci.special.jv(1,parameters.k*an) # first-order bessel function
+    R = np.sqrt((r_m[0]-r_n[0])**2 + (r_m[1]-r_n[1])**2) # distance vector
+    H0_2 = sci.special.hankel2(0, parameters.k * R) # 0th-order, 2nd degree hankel function
     green_d = -1j/2 * np.pi * parameters.k * an * J1 * H0_2
     return green_d
 
@@ -106,7 +119,6 @@ def get_field_data(node):
     Ez = node_data[node].Ez
     return Ez
 
-
 def get_complete_field_data():
     global node_data
 
@@ -116,22 +128,36 @@ def get_complete_field_data():
 
     return field_data
 
-
 def get_grid_coord(node): 
     global node_data
 
     [x,y] = node_data[node].coord
     return [float(x),float(y)]
 
-# TODO: collect antenna locations
-def get_source_coord(id): 
-    # These should be pre-mapped
-    x = 1
-    y = 1
-    return np.array[x,y]
+def get_x_vector(): 
+    global antenna_data
 
-# TODO: what is L vs M? M: # independent measurements, L: # sources?
-# TODO: Fix this
+    x_vec = []
+    for antenna in antenna_data:
+        x_vec.append(float(antenna.coord[0]))
+    return x_vec
+
+def get_y_vector(): 
+    global antenna_data
+
+    y_vec = []
+    for antenna in antenna_data:
+        y_vec.append(float(antenna.coord[1]))
+    return y_vec
+
+
+# TODO: collect antenna locations
+def get_receiver_coord(id): 
+    global antenna_data
+
+    [x,y] = antenna_data[id].coord
+    return [float(x),float(y)]
+
 # TODO: Unit test this function this week; use simple domain
 # This is the Born approximation of the first order, 
 # as it solves the integral equations assuming the incident
@@ -139,38 +165,56 @@ def get_source_coord(id):
 def initial_guess(): # "Lobel et al. (1996) - multifrequency" - Batista
     global node_data
     global parameters
-    global K
+    global K 
     global epsilon
     global Es
 
-    # Estimate permittivity using the min value provided
-    K = np.zeros([parameters.M * parameters.L, parameters.N])
+    # 2, roughly
+    K = np.zeros([parameters.M, parameters.N])
     epsilon = np.zeros([parameters.N, 1]) # Permittivity values
-    Es = np.zeros([parameters.M*parameters.L,1])
+    Es = np.zeros([parameters.M,1])
 
-    G_sum = 0
-    for l in range(1,parameters.L):
-        for m in range(1,parameters.M):
-            for n in range(1,parameters.N): 
-                # Get coordinates of pixel and antenna
-                src_pt = get_grid_coord(n)
-                obs_pt = get_source_coord(m)
+    # G_sum = 0
 
-                # Get field for this pixel 
-                Ez_r = get_field_data(l, n)
+    #     for m in range(1,parameters.M):
+    #         for n in range(1,parameters.N): 
+    #             # Get coordinates of pixel and antenna
+    #             src_pt = get_grid_coord(n)
+    #             obs_pt = get_source_coord(m)
 
-                # integrate over x and y range
-                gs = greens(obs_pt, src_pt)
+    #             # Get field for this pixel 
+    #             Ez_r = get_field_data(l, n)
 
-                # Add Kji element to K matrix 
-                K[m,n] = Ez_r * parameters.k^2 * gs
+    #             # integrate over x and y range
+    #             gs = greens(obs_pt, src_pt)
 
-    # Estimate initial distribution function using zeros vector
-    # TODO: check this
-    epsilon = np.transpose(K) * Es
+    #             # Add Kji element to K matrix 
+    #             K[m,n] = Ez_r * parameters.k**2 * gs
+
+    # # Estimate initial distribution function using zeros vector
+    # # TODO: check this
+    # guess = np.transpose(K) * Es
+
+    # 4
+    
+    # To plot something like 2^ i'll have to map the solution to each coordinate
+    epsilon = np.ones([parameters.N,1]) * parameters.eps_min
+
+    fig = plt.figure()
+    ax = plt.axes(projection = '3d')
+    X = get_x_vector()
+    Y = get_y_vector()
+    ax.plot_surface(X,Y,epsilon)
+    plt.show()
+
+    # 5
+    #guess = parameters.eps_max
+
+    return epsilon
+
 
 # params is a struct containing all required parameters (eg. I, J, Epsilon, etc)
-def run(params: Params, data: np.array(Node)):
+def run(params: Params, data: np.array(Node), antenna_locations: np.array(Antenna)):
     # Assign globals
 
     global K
@@ -178,10 +222,14 @@ def run(params: Params, data: np.array(Node)):
     global epsilon    
     global node_data
     global parameters
+    global antenna_data
 
     node_data = data
     parameters = params 
 
+    gs_k = np.zeros([parameters.M, parameters.N])
+    
+    antenna_data = antenna_locations
     # Perform first-order born approximation to 
     # find initial permittivity solution
     epsilon = initial_guess()
@@ -191,40 +239,37 @@ def run(params: Params, data: np.array(Node)):
     while awaiting_solution:    
         # M: # of transmitters x # of receivers
         # Assume all transmitters are capable of receiving
-        for l in range(1,parameters.L): # Loop over each source (tx)
-            for m in range(1,parameters.M): # Loop over each independent measurement (rx)
+        for m in range(0,parameters.M): # Loop over each independent measurement (rx)
+            # Observation coordinate
+            obs_pt = get_receiver_coord(m) 
 
-                # Observation coordinate
-                obs_pt = get_source_coord(m) # We will have one tx at a time and 11 rx -> M = 11
+            # This loop goes over each element in N = I * J pixels
+            for n in range(0,parameters.N): 
 
-                # This loop goes over each element in N = I * J pixels
-                for n in range(1,parameters.N): 
+                # Find coordinates of pixel
+                src_pt = get_grid_coord(n)
 
-                    # This may not be necessary. Depends on how data is arranged
-                    # Source coordinate
-                    # Find coordinates of pixel
-                    src_pt = get_grid_coord(n)
+                # _________________________________________________________
+                #
+                # Step 2: Solve forward scattering problem (using COMSOL)
+                # _________________________________________________________
+                # Get data for this pixel 
+                Ez = get_field_data(n)
 
-                    # _________________________________________________________
-                    #
-                    # Step 2: Solve forward scattering problem (using COMSOL)
-                    # _________________________________________________________
-                    # Get data for this pixel 
-                    Ez = get_field_data(n)
+                # TODO: check this
+                # Perform discretized surface integral over x and y range
+                # K_sum = 0                    
+                # for x in range(parameters.dx/2, parameters.I - parameters.dx/2, parameters.dx): # Use centers of each pixel
+                #     for y in range(parameters.dy/2, parameters.J - parameters.dy/2, parameters.dy):
+                #         coord = [x,y]
+                #         K_sum = K_sum + greens(obs_pt, coord)
+            
+                # TODO: integrate over x and y range
+                gs = greens(obs_pt, src_pt)
 
-                    # TODO: check this
-                    # Perform discretized surface integral over x and y range
-                    # K_sum = 0                    
-                    # for x in range(parameters.dx/2, parameters.I - parameters.dx/2, parameters.dx): # Use centers of each pixel
-                    #     for y in range(parameters.dy/2, parameters.J - parameters.dy/2, parameters.dy):
-                    #         coord = [x,y]
-                    #         K_sum = K_sum + greens(obs_pt, coord)
-                
-                    # TODO: integrate over x and y range
-                    gs = greens(obs_pt, src_pt)
-
-                    # Add Kji element to K matrix 
-                    K[m,n] = Ez * parameters.k^2 * gs
+                # Add Kji element to K matrix 
+                gs_k[m,n] = parameters.k**2 * gs # store for later
+                K[m,n] = Ez * parameters.k**2 * gs
 
 
         # __________________________________________________________
@@ -233,7 +278,11 @@ def run(params: Params, data: np.array(Node)):
         # ___________________________________________________________
         # Calculate LHS (Total electric field) using previous permittivity solution to solve forward problem
         # b = K * a
-        Es = K * epsilon
+        Es = K.dot(epsilon) # Todo: wrong shape
+
+        # eps: column of length N
+        # Es: column of length M
+        # K : M x N matrix
 
         # ----------------------
         # Perform regularization
@@ -251,23 +300,26 @@ def run(params: Params, data: np.array(Node)):
         # ------------------------------
         # Solve for permittivity profile
         # ------------------------------
-        epsilon = R^-1 * K_t * Es
+        epsilon = R**-1 * K_t * Es  # TODO: is this correct?
 
         # ____________________________
         #
-        # STEP 4: Solve forward problem to recalculate Ez_s
+        # STEP 4: Solve forward problem to recalculate Ez
         # ____________________________
-        # 
-        Es = K * epsilon
-                
+        # b = K * a
+        # K = b / a
+        K_temp = np.transpose(Es / epsilon) # Is this correct? Retain Ez values for next round?
+        Ez_temp = K_temp/gs_k
+
         # ____________________________
         #
         # STEP 5: Compare to measured data
         # ____________________________
         # Calculate error: 
 
-        # TODO: fix this. need to compare all data at once, or piece by piece
-        err = abs((Es - get_complete_field_data())/Es) * 100 # Error (%)
+        Ez_meas = get_complete_field_data()
+
+        err = np.max(abs((Ez_temp)/Ez_meas)) * 100 # Error (%)
 
         if err < 5.0:
             awaiting_solution = False
--- Python/test.py
+++ Python/test.py
@@ -3,7 +3,9 @@ import csv
 import bim_helper as bim
 
 freq = 1e9
-fname = "C:/Users/hanzf/OneDrive/Documents/GitHub/microwave_imaging/bim/Python/pt_2_sig_2.csv"
+fname = "C:/Users/hanzf/OneDrive/Documents/GitHub/microwave_imaging/bim/COMSOL/pt_2.csv"
+
+antenna_coords_fname = "C:/Users/hanzf/OneDrive/Documents/GitHub/microwave_imaging/bim/COMSOL/pt_2.csv"
 
 # Define main function
 def main():
@@ -14,7 +16,7 @@ def main():
     N = 0
     field_data = []
 
-    # Parse the data file line by line
+    # Parse the Ez data file line by line
     with open(fname,'r') as csv_file:
         reader = csv.reader(csv_file)
         i = 0
@@ -37,23 +39,38 @@ def main():
 
         csv_file.close()
 
+    antennas = []
+
+    # These are the grid coordinates used to define the E field at each point
+    # Parse the antenna coordinates file line by line
+    with open(antenna_coords_fname,'r') as csv_file:
+        reader = csv.reader(csv_file)
+        i = 0
+        count = -1      
+        for row in reader:
+            if count > -1:
+                antennas.append(bim.Antenna(count, row[0], row[1]))
+            count = count + 1 # not to exceed N
+         
+        csv_file.close()
+
     print(N)
 
-    if N != I*J:
-        print("N mismatch")
+    #if N != I*J:
+    #    print("N mismatch")
 
     [first_x, first_y] = field_data[0].coord
     [last_x, last_y] = field_data[N-1].coord 
     
-    dx = (float(last_x)-float(first_x)) / (I-1)
-    dy = (float(last_y)-float(first_y)) / (J-1)
+    #dx = (float(last_x)-float(first_x)) / (I-1)
+    #dy = (float(last_y)-float(first_y)) / (J-1)
 
-    print([dx,dy])
+    #print([dx,dy])
 
     #for multiple sources/freqs, iterate here? Could grab data from different pages in excel
     # ^No. Each independent measurement improves accuracy of the solution
-    parameters = bim.Params(freq,dx,dy,I,J) 
-    solution = bim.run(parameters, field_data)
+    parameters = bim.Params(freq,0,0,0,0) 
+    solution = bim.run(parameters, field_data, antennas)
 
     # plot solution
     print(solution)
