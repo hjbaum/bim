Updated gitignore, fixed error calculation

Python/bim_helper.py
@@ -119,7 +119,6 @@ def get_y_vector():
     global incident_data
 
     Y = []
-    y_vec = set(Y)
 
     for point in incident_data:
         x_val = float(point.coord[1])
@@ -181,15 +180,22 @@ def run(params: Params, inc_data: np.array(Node), scat_data: np.array(Node), bas
     scatter_data = scat_data
     parameters = params 
 
+    # Indicates if we are calculating the solution for 
+    #   an empty domain vs the bleed domain
+    running_baseline = False
+
     # Perform first-order born approximation to 
-    # find initial permittivity solution
+    #   find initial permittivity solution
     if np.any(base_solution):
         epsilon = base_solution
+        running_baseline = False
     else:
-        epsilon = initial_guess()  
+        epsilon = initial_guess() 
+        running_baseline = True 
 
     base_epsilon = np.zeros([parameters.N, 1]) 
-    if np.any(base_solution):
+
+    if not running_baseline:
         base_epsilon = base_solution
 
     # Define max number of iterations to compute. Otherwise,
@@ -197,7 +203,7 @@ def run(params: Params, inc_data: np.array(Node), scat_data: np.array(Node), bas
     MAX_ITER = 1
 
     # Define arbitrary regularization param
-    gamma = 0.5 #E-10 # Should be btn 1e-10 and 1e-15
+    gamma = 0.00001 #E-10 # Recommendation between 1e-10 and 1e-15
 
     #fig = plt.figure("Error")
     
@@ -218,9 +224,6 @@ def run(params: Params, inc_data: np.array(Node), scat_data: np.array(Node), bas
             # Observation coordinate
             obs_pt = get_receiver_coord(m) 
 
-            if m == tx_id: 
-                continue
-
             # This loop goes over each element in N = I * J pixels
             for n in range(0,parameters.N): 
                 # _________________________________________________________
@@ -234,7 +237,7 @@ def run(params: Params, inc_data: np.array(Node), scat_data: np.array(Node), bas
                 for p in range(0,parameters.N):
                     # Find coordinates of pixel
                     src_pt = get_grid_coord(p)
-                    gs = greens(obs_pt, src_pt)
+                    gs = greens(obs_pt, src_pt) # Save for reference
                     sum = sum + gs * parameters.dx * parameters.dy
 
                 # Add Kji element to K matrix 
@@ -278,32 +281,25 @@ def run(params: Params, inc_data: np.array(Node), scat_data: np.array(Node), bas
         # STEP 4: Solve forward problem to recalculate Ez
         # ____________________________
         # b = K * a
-        Es_check = K * (epsilon-base_epsilon) # TODO: check
+        Es_check = K.dot(epsilon-base_epsilon) # TODO: check
 
         # ____________________________
         #
         # STEP 5: Compare to measured data
         # ____________________________
         # Calculate error: 
-        scatter_measured = get_scatter_measurements()
-        err_array = Es_check.transpose()/scatter_measured
-        ave_error = abs(np.average(err_array.transpose()))
-        percent_err = ave_error * 100 # Error (%)
-        print("Error: ", str(percent_err), "%")
+
+        if not running_baseline:
+            scatter_measured = get_scatter_measurements()
+            err_array = (Es_check.transpose() - scatter_measured) / scatter_measured
+            ave_error = abs(np.average(err_array.transpose()))
+            percent_err = ave_error * 100 # Error (%)
+            print("Error: ", str(percent_err), "%")
         
-        #if np.any(base_solution):
-        #    fig = plt.figure("Error")
-        #    plt.scatter(err_array)
-        #    plt.show()
-
-        # Plot error vs iteration
-        #plt.scatter(iteration, err)
-        #plt.pause(0.0001)
-
-        if percent_err < 5.0:
-            print("Error threshold met!")
-            awaiting_solution = False
-            break
+            if percent_err < 5.0:
+                print("Error threshold met!")
+                awaiting_solution = False
+                break
 
 
     final = epsilon - base_epsilon

Python/test.py
@@ -4,6 +4,7 @@ import bim_helper as bim
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+import scipy.io
 from mpl_toolkits import mplot3d
 import os
 
@@ -119,14 +120,12 @@ def main():
     fig = plt.figure("Solution")
     ax = plt.axes(projection = '3d')
     colors = ['blue', 'green']
-    cmap_temp = ['hot', 'winter']
 
     # Parse the csv for the source files containing field data
     with open(files_by_source_csv,'r') as csv_file:
         reader = csv.reader(csv_file)    
         row_count = 0   
 
-        
         for row in reader:
             row_count = row_count + 1
             if row_count > 1:
@@ -134,38 +133,43 @@ def main():
                 source_id = row[0]  
 
                 # Row format:
-                # Col 1     Col 2         Col 3                   Col 4                     Col 5                      Col 6
-                # Source #  Folder name   Scatter file for bleed  Incident file for bleed   Scatter file for baseline  Incident file for baseline
+                # Col 0         Col 1           Col 2                       
+                # Source ID     Alternate ID    Folder name     
+                #
+                # Col 3                         Col 4                     
+                # Scatter file for bleed        Incident file for bleed   
+                # 
+                # Col 5                         Col 6
+                # Scatter file for baseline     Incident file for baseline
 
                 # with bleed
-                bleed_scatter_field_fname = os.path.join(dirname, '..', 'COMSOL', row[1], row[2])
-                bleed_incident_field_fname =  os.path.join(dirname, '..', 'COMSOL', row[1], row[3])
+                bleed_scatter_field_fname = os.path.join(dirname, '..', 'COMSOL', row[2], row[3])
+                bleed_incident_field_fname =  os.path.join(dirname, '..', 'COMSOL', row[2], row[4])
                                                            
                 # baseline domain
-                scatter_field_fname = os.path.join(dirname, '..', 'COMSOL', row[1], row[4])
-                incident_field_fname = os.path.join(dirname, '..', 'COMSOL', row[1], row[5])
+                scatter_field_fname = os.path.join(dirname, '..', 'COMSOL', row[2], row[5])
+                incident_field_fname = os.path.join(dirname, '..', 'COMSOL', row[2], row[6])
 
                 # Solve base field
                 baseline_solution = solve_baseline(incident_field_fname, scatter_field_fname, int(source_id))
-                # Plot absolute value; currently dropping imag component
+                # Plot absolute value instead of dropping imag component
                 #ax.scatter(X, Y, abs(baseline_solution), label=legend, color=colors[row_count-2], lw=0.5)    
                 
                 # Solve field of interest
                 solution = solve_bleed(bleed_incident_field_fname, bleed_scatter_field_fname, baseline_solution, int(source_id))
 
+                X = bim.get_x_vector()
+                Y = bim.get_y_vector()
+
+                name = 'epsilon_src' + str(source_id) + '.mat'
+
+                scipy.io.savemat(name, dict(x=X, y=Y, solution=solution))
+
                 # Plot solution
                 legend = 'Source ' + str(source_id)
 
-                X = bim.get_x_vector()
-                Y = bim.get_y_vector()
                 # Plot absolute value; currently dropping imag component
                 ax.scatter(X, Y, abs(solution), label=legend, color=colors[row_count-2], lw=0.5)    
-                
-                #my_cmap = plt.get_cmap(cmap_temp[row_count-2])
-                #surf = ax.plot_surface(X,Y,solution, cmap=my_cmap, edgecolor='none', label=legend)
-                #surf._edgecolors2d = surf._edgecolor3d
-                #surf._facecolors2d = surf._facecolor3d
-                #fig.colorbar(surf, ax=ax)
                 
                 break
                 